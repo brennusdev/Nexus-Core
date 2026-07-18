390a2f7a00aa6eb27b6d5e14579f4a39d743d446
Brenno Pereira <brennuscontato@gmail.com>
2026-07-17 23:44:22 -0300

    Atualizando graphic em java

--- backend/java.graphic/javaGraphic.java
+++ backend/java.graphic/javaGraphic.java
@@ -1,6 +1,9 @@
 public class javaGraphic{
     static public void main(String[] args){
         System.out.println("Bem-vindo ao seu gráfico!");
+
+        
+        System.out.println("Este é um gráfico em Java.");
     }
  }
 
