a581820326e9bfe2e86bfe848931cac64ef427a4
Brenno Pereira <brennuscontato@gmail.com>
2026-07-20 15:53:34 -0300

    Inserindo estudo.java

---
+++ nexus-core-web  FRT/Estudo.java
@@ -0,0 +1,19 @@
+public class Estudo {
+
+    private String linguagem;
+    private int horas;
+
+    public Estudo(String linguagem, int horas) {
+        this.linguagem = linguagem;
+        this.horas = horas;
+    }
+
+    public String getLinguagem() {
+        return linguagem;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+}
