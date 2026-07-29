9a3db92263f83f761b058eea6140f8ab9aeb33d3
Brenno Pereira <brennuscontato@gmail.com>
2026-07-29 14:31:46 -0300

    Atualizado user

---
+++ Backend/App/models/user.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import DateTime
+
+from datetime import datetime
+
+from App.core.database import Base
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    name = Column(
+        String(100),
+        nullable=False
+    )
+
+    email = Column(
+        String(255),
+        unique=True,
+        nullable=False
+    )
+
+    password = Column(
+        String(255),
+        nullable=False
+    )
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
