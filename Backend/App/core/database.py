56b652ae1e3222d6217ba1dd64c36e91c3efd365
Brenno Pereira <brennuscontato@gmail.com>
2026-07-29 14:31:56 -0300

    Database atualizada

---
+++ Backend/App/core/database.py
@@ -0,0 +1,16 @@
+import os
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALGORITHM = os.getenv("ALGORITHM")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+)
